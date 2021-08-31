474cdcdaa6c60ba5a39c873065828cc69d44af59
Elton <eltonfernz54@gmail.com>
2021-08-31 21:41:46 +0530

    Elton

--- FatherMuller/assets/js/regpatient.js
+++ FatherMuller/assets/js/regpatient.js
@@ -1,93 +1,92 @@
-$(document).ready(function() {  
+$(document).ready(function () {
     var res = "";
     $("#single").hide();
     $("#family").hide();
     $("#common").hide();
-    $("#Add").on("click", function() {  
-        $("#textboxDiv").append("<div><h3>New Patient</h3><br><label>Firstname:</label><input class='form-control firstN' type='text'/><br><label>Lastname:</label><input class='form-control lastN' type='text'/><br><label>Gender :</label><br><div class='form-group'><div class=''><form><select class='form-control gendr' id='Gender' name='gender[]'><option value=''id='optionsRadios1'>Male</option><option value=''id='optionsRadios2'>Female</option><option value=''id='optionsRadios3'>Other</option></select></form></div></div><br><label>Birthday:</label><input class='form-control brthdy' type='date' id='birthday' name='birthday'></div><br><div><label>Symptoms :</label><textarea class='form-control Sympt' value='symptoms' required></textarea></div><br>");  
-        });  
-    $("#Remove").on("click", function() {  
-        $("#textboxDiv").children().last().remove();  
-    });  
-    $("#okay").click(function(){
+    $("#Add").on("click", function () {
+        $("#textboxDiv").append("<div><h3>New Patient</h3><br><label>Firstname:</label><input class='form-control firstN' type='text'/><br><label>Lastname:</label><input class='form-control lastN' type='text'/><br><label>Gender :</label><br><div class='form-group'><div class=''><form><select class='form-control gendr' id='Gender' name='gender[]'><option value='Male'id='optionsRadios1'>Male</option><option value='Female'id='optionsRadios2'>Female</option><option value='Other'id='optionsRadios3'>Other</option></select></form></div></div><br><label>Birthday:</label><input class='form-control brthdy' type='date' id='birthday' name='birthday'></div><br><div><label>Symptoms :</label><textarea class='form-control Sympt' value='symptoms' required></textarea></div><br>");
+    });
+    $("#Remove").on("click", function () {
+        $("#textboxDiv").children().last().remove();
+    });
+    $("#okay").click(function () {
         displayRadioValue();
-      });
-      $("#submit").click(function(){
-         SinglePatient();
-        });
-        $("#submit2").click(function(){
-            Family();
-           });
-});  
-function SinglePatient(){
+    });
+    $("#submit").click(function () {
+        SinglePatient();
+    });
+    $("#submit2").click(function () {
+        Family();
+    });
+});
+function SinglePatient() {
     var pid = $("#pid").val();
-    var fname= $("#fname").val();
-    var lname= $("#lname").val();
-   var gender= $('#Gender option:selected').text();
-    var birthday= $("#birthday").val();
-    var adrs= $("#adrs").val();
-    var cty= $("#cty").val();
-    var phne= $("#phne").val();
-    var sympt= $("#sympt").val();
-    if(pid==""||fname==""||lname==""||gender==""||birthday==""||adrs==""||cty==""||phne==""||sympt=="")
-    {
+    var fname = $("#fname").val();
+    var lname = $("#lname").val();
+    var gender = $('#Gender option:selected').text();
+    var birthday = $("#birthday").val();
+    var adrs = $("#adrs").val();
+    var cty = $("#cty").val();
+    var phne = $("#phne").val();
+    var sympt = $("#sympt").val();
+    if (pid == "" || fname == "" || lname == "" || gender == "" || birthday == "" || adrs == "" || cty == "" || phne == "" || sympt == "") {
         swal("Warning!", "Please fill all the fields!", "warning");
-    }else{
+    } else {
         $.ajax({
             type: "POST",
             url: "../../assets/php/regpatient.php",
             data: {
-              pid:pid,
-              fname:fname,
-              lname:lname,
-              gender:gender,
-              birthday:birthday,
-              adrs:adrs,
-              cty:cty,
-              phne:phne,
-              sympt:sympt,
+                pid: pid,
+                fname: fname,
+                lname: lname,
+                gender: gender,
+                birthday: birthday,
+                adrs: adrs,
+                cty: cty,
+                phne: phne,
+                sympt: sympt,
 
             },
             success: function (response) {
-              var jsonData = JSON.parse(response);
-              if (jsonData.status === "passwordError") {
-                alert(jsonData.status);
-                swal("Warning","Password Error","warning");
-                $(".passwordError").html(jsonData.message);
-              } else if (jsonData.status === "emailError") {
-                alert(jsonData.status);
-                swal("Warning!", "Username is Incorrect", "warning");
-                $(".emailError").html(jsonData.message);
-              } else if (jsonData.status === "success") {
-                window.location = "../../index1.html";
-               }
+                var jsonData = JSON.parse(response);
+                if (jsonData.status === "passwordError") {
+                    alert(jsonData.status);
+                    swal("Warning", "Password Error", "warning");
+                    $(".passwordError").html(jsonData.message);
+                } else if (jsonData.status === "emailError") {
+                    alert(jsonData.status);
+                    swal("Warning!", "Username is Incorrect", "warning");
+                    $(".emailError").html(jsonData.message);
+                } else if (jsonData.status === "success") {
+                    window.location = "../../index1.html";
+                }
             },
-          });
-    }   
+        });
+    }
 }
 
-function Family(){
+function Family() {
     var arr = [];
-$("input.firstN").each(function(){
-    arr.push($(this).val()); 
-});
-$("input.lastN").each(function(){
-    arr.push($(this).val()); 
-});
-$(".gendr option:selected").each(function(){
-    arr.push($(this).val()); 
-});
-$("input.brthdy").each(function(){
-    arr.push($(this).val()); 
-});
-$("textarea.Sympt").each(function(){
-    arr.push($(this).val()); 
-});
+    $("input.firstN").each(function () {
+        arr.push($(this).val());
+    });
+    $("input.lastN").each(function () {
+        arr.push($(this).val());
+    });
+    $(".gendr option:selected").each(function () {
+        arr.push($(this).val());
+    });
+    $("input.brthdy").each(function () {
+        arr.push($(this).val());
+    });
+    $("textarea.Sympt").each(function () {
+        arr.push($(this).val());
+    });
 }
 // function displayRadioValue() {
 //     var ele = document.getElementsByName('gender');
 //     var res = "";
-      
+
 //     for(i = 0; i < ele.length; i++) {
 //         if(ele[i].checked)
 //         res = ele[i].value;
@@ -95,13 +94,13 @@ $("textarea.Sympt").each(function(){
 //     }
 // }
 function displayRadioValue() {
-	
+
     var ele = document.getElementsByName('Patient');
-    
-    
-    for(i = 0; i < ele.length; i++) {
-        if(ele[i].checked)
-        res = ele[i].value;
+
+
+    for (i = 0; i < ele.length; i++) {
+        if (ele[i].checked)
+            res = ele[i].value;
     }
     // $.ajax({
     //     type: "POST",
@@ -120,25 +119,25 @@ function displayRadioValue() {
     //        }
     //     },
     //   });
-    
-    if(res==""){
+
+    if (res == "") {
         alert("Select any one of the option");
     }
-    else if(res=="Single Patient"){
-    $("#single").show();
-    $("#sngl").show();
-    $("#fmly").hide();
-    $("#family").hide();
-    $("#common").show();
-    $("#submit").show();
-    $("#submit2").hide();
-    }else{
-    $("#single").show();
-    $("#sngl").hide();
-    $("#fmly").show();
-    $("#family").show();
-    $("#common").show();
-    $("#submit").hide();
-    $("#submit2").show();
+    else if (res == "Single Patient") {
+        $("#single").show();
+        $("#sngl").show();
+        $("#fmly").hide();
+        $("#family").hide();
+        $("#common").show();
+        $("#submit").show();
+        $("#submit2").hide();
+    } else {
+        $("#single").show();
+        $("#sngl").hide();
+        $("#fmly").show();
+        $("#family").show();
+        $("#common").show();
+        $("#submit").hide();
+        $("#submit2").show();
     }
 }
